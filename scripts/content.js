1eb8f93ded8194d0e23f484ba2dace085900539e
Viviane Qian <viviane@diverta.co.jp>
2023-07-04 17:20:38 +0900

    toggle annotations

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,18 @@
 const imgTags = document.getElementsByTagName('img');
 
+const translatedImgHandler = async (img) => {
+  // toggle annotations
+  const existingAnnotations = img.parentNode.getElementsByClassName(
+    'annotationsContainer'
+  )[0];
+  if (existingAnnotations) {
+    existingAnnotations.style.display =
+      existingAnnotations.style.display === 'none' ? 'block' : 'none';
+  }
+};
+
 const imgClickHandler = async (img) => {
+  // prepare body for request
   const targetLang = await chrome.storage.sync.get('targetLang');
   const imageSrc = await fetch(img.src);
   const imageBlob = await imageSrc.blob();
@@ -12,22 +24,30 @@ const imgClickHandler = async (img) => {
     };
   });
 
-  const res = await fetch(`https://viviane-internship-app.s3.kuroco-edge.jp/translate`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: JSON.stringify({
-      binaryImage: btoa(binaryImage),
-      targetLang: targetLang,
-    }),
-  })
+  // send request
+  const res = await fetch(
+    `https://viviane-internship-app.s3.kuroco-edge.jp/translate`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: JSON.stringify({
+        binaryImage: btoa(binaryImage),
+        targetLang: targetLang,
+      }),
+    }
+  );
+
   // parse response
   const text = await res.text();
   const parser = new DOMParser();
   const doc = parser.parseFromString(text, 'text/html');
+
   // display annotations on top of image
-  const annotationsContainer = doc.getElementById('annotationsContainer');
+  const annotationsContainer = doc.getElementsByClassName(
+    'annotationsContainer'
+  )[0];
   const div = document.createElement('div');
   div.style.position = 'relative';
   div.style.width = 'fit-content';
@@ -35,6 +55,9 @@ const imgClickHandler = async (img) => {
   div.appendChild(img.cloneNode());
   div.appendChild(annotationsContainer);
   img.replaceWith(div);
+
+  // add click handler to translated image to toggle annotations
+  div.addEventListener('click', () => translatedImgHandler(div));
 };
 
 for (let img of imgTags) {
