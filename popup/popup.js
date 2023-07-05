1d93de7117b217dcfdf520a2066c7536bb8979fd
Viviane Qian <viviane@diverta.co.jp>
2023-07-05 15:17:51 +0900

    load all supported languages

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,23 @@
+const targetLang = document.getElementById('target-lang');
+
 function setTargetLang() {
-  const targetLang = document.getElementById('target-lang').value;
-  chrome.storage.sync.set({ targetLang: targetLang });
+  const targetLangValue = targetLang.value;
+  chrome.storage.sync.set({ targetLang: targetLangValue });
 }
 
-function getTargetLang() {
+async function getTargetLang() {
+  // load target lang options
+  const res = await fetch(
+    'https://viviane-internship-app.s3.kuroco-edge.jp/'
+  )
+  const text = await res.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/html');
+  targetLang.innerHTML = doc.getElementById('target-lang').innerHTML;
+  
+  // set target lang to last selected value
   chrome.storage.sync.get('targetLang', function (data) {
-    document.getElementById('target-lang').value = data.targetLang;
+    targetLang.value = data.targetLang;
   });
 }
 
